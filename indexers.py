f5f45920af81839552e9a33af4184cb3649b5b5d
Shivam Garg <sgshivamgarg8@gmail.com>
2024-07-29 20:44:52 +0530

    feat: add venv

--- indexers.py
+++ indexers.py
@@ -1,19 +1,20 @@
 import requests
 import markdown
 from bs4 import BeautifulSoup
+
+
 def scrape_trackers():
     url = "https://raw.githubusercontent.com/Shakil-Shahadat/awesome-piracy/main/Readme.md"
-    all_trackers=[]
+    all_trackers = []
     response = requests.get(url)
     md_text = response.text
     md_html = markdown.markdown(md_text)
-    
-    soup = BeautifulSoup(md_html, 'html.parser')
-    
+
+    soup = BeautifulSoup(md_html, "html.parser")
+
     for heading in soup.find_all("h4"):
-        if(heading.text=="Public Trackers"):
+        if heading.text == "Public Trackers":
             next_element = heading.find_next_sibling()
             for link in next_element.find_all("a"):
-                all_trackers.append(link.get("href"))    
-    return all_trackers  
-
+                all_trackers.append(link.get("href"))
+    return all_trackers
